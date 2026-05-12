8c83bb72e7116b9286db2345d25b637bfd92627b
johnhain-msft <johnhhain.work@gmail.com>
2026-05-12 19:20:33 -0400

    ADD: TurnCompletionObserver shared type for chat turn observation

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,3 +4,4 @@ export { IPC, type IpcChannel } from './ipc-channels';
 export { parseIpcArgs } from './ipc-validation';
 export { Logger, type LogLevel } from './logger';
 export { escapeXml } from './escapeXml';
+export type { CompletedTurn, TurnCompletionObserver, TurnStatus } from './turn-observer';
